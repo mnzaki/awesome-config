28337b8a48d7114e8b69f20d28069a2f623deeda
Mina Nagy Zaki <mnzaki@gmail.com>
2015-11-12 01:57:12 +0200

    xchat floats

--- rc.lua
+++ rc.lua
@@ -408,6 +408,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { name = "dmenu-dict" },
       properties = { floating = true } },
+    { rule = { class = "Xchat" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
