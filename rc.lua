776dbdda9014cc02fc133a533b31ca47ea15c659
Mina Nagy Zaki <mnzaki@gmail.com>
2021-04-13 12:59:34 +0200

    mod+shift+t to toggle sticky

--- rc.lua
+++ rc.lua
@@ -363,6 +363,8 @@ clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+              {description = "toggle sticky", group = "client"}),
+    awful.key({ modkey,  "Shift"  }, "t",      function (c) c.sticky = not c.sticky          end,
               {description = "toggle keep on top", group = "client"}),
     awful.key({ modkey,           }, "n",
         function (c)
